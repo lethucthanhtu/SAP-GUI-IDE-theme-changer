ecc0c4714eb024c048b6f01b11431039d56387f1
le tu <lethucthanhtu@gmail.com>
2025-03-04 22:02:51 +0700

    chore: add technical comment

--- SAP_theme_changer.ps1
+++ SAP_theme_changer.ps1
@@ -12,19 +12,28 @@ try {
     exit 1
 }
 
+# Number of theme files
 $FILES_LENGTH = $FILES.Count
+
+# Offset for default option
 $FILES_OFFSET = 1
 
+# Format file name to display name
 function Format-Name {
     param ($filePath)
     return ($filePath -replace "_theme.xml", "" -replace "_", " ")
 }
 
+# Handle theme change
 function Change-Theme {
     param ($selectedFile)
     try {
+        # Get user selected theme file
         $themeUrl = "$THEMES_URL/$selectedFile"
+
+        # Change theme
         Invoke-WebRequest -Uri $themeUrl -OutFile $SOURCE_DIR
+
         Write-Host "Changed theme to $(Format-Name $selectedFile) successfully!"
         Write-Host "Please restart SAP GUI for changes to take effect."
     } catch {
@@ -38,12 +47,12 @@ if ($FILES_LENGTH -eq 0) {
     exit 1
 }
 
+# Main menu
 while ($true) {
     Write-Host "======================="
     Write-Host "[0] Exit program"
     Write-Host "-----------------------"
     Write-Host "Available themes:"
-
     for ($i = 0; $i -lt $FILES_LENGTH; $i++) {
         Write-Host "[$($i + $FILES_OFFSET)] $(Format-Name $FILES[$i]) theme"
     }
@@ -51,6 +60,7 @@ while ($true) {
 
     $choice = Read-Host "Enter your option"
 
+    # Validate user input
     if ($choice -match "^[0-9]+$" -and
         $choice -ge 0 -and
         $choice -le ($FILES_LENGTH + $FILES_OFFSET)) {
@@ -61,12 +71,14 @@ while ($true) {
     }
 }
 
+# Handle user choice
 switch ($choice) {
-    0 { exit 0 }  # Change exit code to 0 for a graceful exit
+    0 { exit 0 }
     default {
         $selectedFile = $FILES[$choice - $FILES_OFFSET]
         Change-Theme $selectedFile
     }
 }
 
+# Wait for user input before exit
 Read-Host "Press any key to exit"
